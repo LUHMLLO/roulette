a8cdce7af0e12001790d40d658477917e257d4db
LUHMLLO <luhmllo06@protonmail.com>
2023-04-09 12:58:14 -0400

    working on roulette

--- src/lib/stores.ts
+++ src/lib/stores.ts
@@ -1,9 +1,52 @@
 import { writable } from "svelte/store";
-import type { Action, Gamemode, Player } from "$lib/interfaces"
+import { Difficulty, type Action, type Gamemode, type Player } from "$lib/interfaces"
 
-//export const gamemode = writable<Gamemode | null>()
-export const players = writable<Player[]>([])
+export const gamemodes = writable<Gamemode[]>([])
+export const isGameStarted = writable<boolean>(false)
 
 export const choosenPlayer = writable<Player>()
 export const choosenAction = writable<Action>()
-export const isGameStarted = writable<boolean>(false)
+
+export const players = writable<Player[]>([])
+export const actions = writable<Action[]>([
+    {
+        difficulty: Difficulty.Easy,
+        description: "Do 20 jumping jacks",
+    },
+    {
+        difficulty: Difficulty.Easy,
+        description: "Do a silly dance",
+    },
+    {
+        difficulty: Difficulty.Easy,
+        description: "Tell a joke",
+    },
+    {
+        difficulty: Difficulty.Medium,
+        description: "Sing a song in a different language",
+    },
+    {
+        difficulty: Difficulty.Medium,
+        description: "Say the alphabet backwards",
+    },
+    {
+        difficulty: Difficulty.Medium,
+        description: "Do 10 push-ups",
+    },
+    {
+        difficulty: Difficulty.Medium,
+        description: "Talk like a pirate for the rest of the game",
+    },
+    {
+        difficulty: Difficulty.Medium,
+        description: "Do an impression of your favorite celebrity",
+    },
+    {
+        difficulty: Difficulty.Hard,
+        description: "Wear your clothes backwards for the rest of the game",
+    },
+    {
+        difficulty: Difficulty.Hard,
+        description: "Take a shot of hot sauce",
+    },
+])
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,55 +1,11 @@
 <script lang="ts">
-    import { Difficulty, type Action } from "$lib/interfaces";
-
     import {
         choosenAction,
         choosenPlayer,
         isGameStarted,
         players,
+        actions,
     } from "$lib/stores";
-
-    const actions: Action[] = [
-        {
-            difficulty: Difficulty.Easy,
-            description: "Do 20 jumping jacks",
-        },
-        {
-            difficulty: Difficulty.Medium,
-            description: "Sing a song in a different language",
-        },
-        {
-            difficulty: Difficulty.Medium,
-            description: "Say the alphabet backwards",
-        },
-        {
-            difficulty: Difficulty.Easy,
-            description: "Do a silly dance",
-        },
-        {
-            difficulty: Difficulty.Hard,
-            description: "Wear your clothes backwards for the rest of the game",
-        },
-        {
-            difficulty: Difficulty.Medium,
-            description: "Do 10 push-ups",
-        },
-        {
-            difficulty: Difficulty.Easy,
-            description: "Tell a joke",
-        },
-        {
-            difficulty: Difficulty.Medium,
-            description: "Talk like a pirate for the rest of the game",
-        },
-        {
-            difficulty: Difficulty.Medium,
-            description: "Do an impression of your favorite celebrity",
-        },
-        {
-            difficulty: Difficulty.Hard,
-            description: "Take a shot of hot sauce",
-        },
-    ];
 
     let inputPlayer: string;
 
@@ -64,44 +20,66 @@
 
     function startGame() {
         const randomPlayer = Math.floor(Math.random() * $players.length);
-        const randomAction = Math.floor(Math.random() * actions.length);
+        const randomAction = Math.floor(Math.random() * $actions.length);
+
         choosenPlayer.set($players[randomPlayer]);
-        choosenAction.set(actions[randomAction]);
+        choosenAction.set($actions[randomAction]);
+
         isGameStarted.set(true);
     }
 </script>
 
-<section class="game">
-    <h1>Player List</h1>
+<grid class="place--center h--100vh">
+    <container class="container">
+        <h1>Greetings</h1>
+        <p>
+            Aliquid ex delectus maiores, nam sit reiciendis placeat ratione
+            blanditiis, laudantium vel ipsum modi fuga molestias voluptatibus
+            laboriosam debitis est sed assumenda.
+        </p>
+        <small>Lorem ipsum dolor sit amet consectetur adipisicing elit.</small>
+        <button>Let's go</button>
+    </container>
+</grid>
 
-    <form on:submit|preventDefault={addPlayer}>
-        <label for="name">Enter a player name:</label>
-        <input type="text" id="name" bind:value={inputPlayer} />
-        <button type="submit">Add player</button>
-    </form>
+<grid class="place--center h--100vh">
+    <container class="container">
+        <form on:submit|preventDefault={addPlayer}>
+            <label for="name">Enter a player name:</label>
+            <input type="text" id="name" bind:value={inputPlayer} />
+            <button type="submit">Add player</button>
+        </form>
+    </container>
+</grid>
 
-    {#if $players.length > 0}
-        <div class="player-section">
+{#if $players.length > 0}
+    <grid class="place--center h--100vh">
+        <container class="container">
             <h2>Players:</h2>
             <ul>
                 {#each $players as player}
                     <li>{player.name}</li>
                 {/each}
             </ul>
-        </div>
 
-        <button
-            on:click={startGame}
-            disabled={$players.length < 2 || actions.length < 1}
-            >Start game</button
-        >
-    {/if}
+            <button
+                on:click={startGame}
+                disabled={$players.length < 2 || $actions.length < 1}
+            >
+                Start game
+            </button>
+        </container>
+    </grid>
+{/if}
 
-    {#if $isGameStarted}
-        <div class="gameplay-section">
+{#if $isGameStarted}
+    <grid class="place--center h--100vh">
+        <container class="container">
             <h2>Gameplay:</h2>
             <p>Selected player: {$choosenPlayer.name}</p>
             <p>Selected dare: {$choosenAction.description}</p>
-        </div>
-    {/if}
-</section>
+
+            <button on:click={startGame}> Next </button>
+        </container>
+    </grid>
+{/if}
