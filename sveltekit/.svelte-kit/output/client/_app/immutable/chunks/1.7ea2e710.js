59df0e022f357466416c02fb4f20ece3ca1ab487
LUHMLLO <luhmllo06@protonmail.com>
2023-04-14 20:54:42 -0400

    manually deleting .svelte-kit folder

--- sveltekit/.svelte-kit/output/client/_app/immutable/chunks/1.7ea2e710.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.c4b64e81.js";export{t as component};
--- sveltekit/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-export const component = async () => (await import('../entries/pages/_page.svelte.js')).default;
-export const file = '_app/immutable/entry/_page.svelte.c66c499a.js';
-export const imports = ["_app/immutable/entry/_page.svelte.c66c499a.js","_app/immutable/chunks/index.b427a141.js","_app/immutable/chunks/stores.49592188.js","_app/immutable/chunks/index.b8e72a41.js"];
-export const stylesheets = [];
-export const fonts = [];
