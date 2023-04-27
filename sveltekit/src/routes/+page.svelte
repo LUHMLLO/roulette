b31b5905b6af1b0a1668bfa84ceebf703f03c7cd
LUHMLLO <luhmllo06@protonmail.com>
2023-04-27 14:28:15 -0400

    updated sveltekit version since its the one that im going to be using

--- sveltekit/src/routes/+page.svelte
+++ sveltekit/src/routes/+page.svelte
@@ -14,29 +14,26 @@
 
 {#if !$game.screen}
     <grid class="place-center h-100vh">
-        <container class="container text-center">
+        <container class="text-center">
             <h3 class="clr-accent">Bonjour!</h3>
-            <h1 class="clr-secondary">Bienvenue to Roulette.</h1>
-            <p class="clr-tertiary text--24">
+            <h1>Bienvenue to Roulette.</h1>
+            <p class="mb-16">
                 Let's see if Lady Luck is on your side today.
                 <br />
                 With so many possibilities, who knows what the outcome will be!
             </p>
-            <button
-                class="theme-radius my--24"
-                on:click={() => ($game.screen = 1)}>Start playing</button
-            >
+            <button on:click={() => ($game.screen = 1)}>Start playing</button>
         </container>
     </grid>
 {/if}
 
 {#if $game.screen == 1}
     <grid class="place-center h-100vh">
-        <container class="container text-center">
+        <container class="text-center">
             <h3>Who's going to play?</h3>
 
             <form
-                class="flex row bord-secondary bord--solid bord-width--2 theme-radius overflow--hidden my--24"
+                class="row align-center no-overflow"
                 on:submit|preventDefault={() => {
                     addPlayer(
                         undefined,
@@ -48,18 +45,19 @@
             >
                 <input
                     type="text"
-                    class="bord--hidden"
+                    name="playername"
                     bind:value={inputPlayerName}
                 />
-                <button class="p-0 m--0 bord--hidden">
-                    <icon class="icon size--32 bg-accent clr-primary"
-                        ><i class="iconoir-plus" /></icon
-                    >
+
+                <button type="submit">
+                    <icon class="size-32">
+                        <i class="iconoir-plus" />
+                    </icon>
                 </button>
             </form>
 
             {#if $players.length > 0}
-                <row class="flex row justify--center align--center gap-16">
+                <row class="justify-center align-center gap-16">
                     {#each $players as player}
                         <h6>{player.name}</h6>
                     {/each}
@@ -67,7 +65,6 @@
             {/if}
 
             <button
-                class="theme-radius my--24"
                 disabled={$players.length < 2 || $players.length < 1}
                 on:click={() => {
                     startGame();
@@ -80,12 +77,11 @@
 {#if $game.screen == 2}
     {#if $game.onGoing}
         <grid class="place-center h-100vh">
-            <container class="container text-center">
+            <container class="text-center">
                 <h1 class="clr-accent">{$choosenPlayer.name}</h1>
-                <p class="text--24">{$choosenAction.description}</p>
+                <p class="mb-16">{$choosenAction.description}</p>
 
                 <button
-                    class="theme-radius my--24"
                     disabled={$players.length < 2 || $players.length < 1}
                     on:click={() => {
                         nextRound();
